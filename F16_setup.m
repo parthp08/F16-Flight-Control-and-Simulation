8ef45d2c2fc701106f528f3e291dc43328055136
Parth Viradiya <parthviradiya08@gmail.com>
2020-11-22 00:47:40 -0500

    Initial commits

---
+++ F16_setup.m
@@ -0,0 +1,38 @@
+% F16 Aircraft Parameters   
+% Global Variables
+% Ref: Aircraft Control and Simulation, Stevens & Lewis.
+
+clear all;
+close all;
+clc;
+
+global m g Ib Ib_inv S c_bar b xcg_ref x_cg;
+
+m = 9298.6436; % kg
+g = 9.81;   % m/s^2
+S = 27.8709; % wing area, m^2
+c_bar = 3.450336; % mean aero cord, m
+b = 9.144; % wing span, m
+xcg_ref = 0.35*c_bar; % reference c.g. location
+x_cg = 0.35*c_bar; % c.g. location % VARIABLE
+
+% inertia matrix    % kg*m^2    
+Ib = [12874.85, 0.0, 1331.41;
+    0.0, 75673.62, 0.0;
+    1331.41, 0.0, 85552.11];
+
+% inverse inertia matrix
+% precomputed for faster executions
+Ib_inv = 1e-4 * [
+    0.7780         0   -0.0121;
+         0    0.1321         0;
+   -0.0121         0    0.1171;
+];
+
+
+% initial states
+
+
+% control saturation
+
+
---
+++ atmospheric_model.m
@@ -0,0 +1,73 @@
+function rho = atmospheric_model(altitude)
+% calculate the atmospheric density(in Kg/m^3) for the 
+% given altitude
+% 
+% Ref: U.S. Standard Atmosphere, 1976
+
+global g;
+R_air = 287.05287;  % Specific gas constant(for dry air), J/Kg·K
+
+if (0 <= altitude) && (altitude < 11000)
+    alpha = -0.0065;  % K/m
+    T0 = 288.15;      % K
+    p0 = 101325.0;    % Pa
+    
+    T = T0 + alpha * altitude;
+    p = p0 * (T0 / T) ^ (g / (R_air * alpha));
+    
+elseif (11000 <= altitude) && (altitude < 20000)
+    T = 216.65;    % K
+    p0 = 22632.1;  % Pa
+    h0 = 11000;    % m
+
+    p = p0 * exp(-g * (altitude - h0) / (R_air * T));
+
+elseif (20000 <= altitude) && (altitude < 32000)
+    alpha = 0.001;    % K/m
+    T0 = 216.65;      % K
+    p0 = 5474.89;     % Pa
+    h0 = 20000;       % m
+
+    T = T0 + alpha * (altitude - h0);
+    p = p0 * (T0 / T) ^ (g / (R_air * alpha));
+
+elseif (32000 <= altitude) && (altitude < 47000)
+    alpha = 0.0028;   % K/m
+    T0 = 228.65;      % K
+    p0 = 868.019;     % Pa
+    h0 = 32000;       % m
+
+    T = T0 + alpha * (altitude - h0);
+    p = p0 * (T0 / T) ^ (g / (R_air * alpha));
+
+elseif (47000 <= altitude) && (altitude < 51000)
+    T = 270.65;    % K
+    p0 = 110.906;  % Pa
+    h0 = 47000;    % m
+
+    p = p0 * exp(-g * (altitude - h0) / (R_air * T));
+
+elseif (51000 <= altitude) && (altitude < 71000)
+    alpha = -0.0028;  % K/m
+    T0 = 270.65;      % K
+    p0 = 66.9389;     % Pa
+    h0 = 51000;       % m
+
+    T = T0 + alpha * (altitude - h0);
+    p = p0 * (T0 / T) ^ (g / (R_air * alpha));
+
+elseif (71000 <= altitude) && (altitude < 84500)
+    alpha = -0.002;   % K/m
+    T0 = 214.65;      % K
+    p0 = 3.95642;     % Pa
+    h0 = 71000;       % m
+
+    T = T0 + alpha * (altitude - h0);
+    p = p0 * (T0 / T) ^ (g / (R_air * alpha));
+
+else
+    error("altitude must be below 84500 m");
+end
+
+rho = p / (R_air * T);
+end
