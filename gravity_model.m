c2faa40b4d02dd69085ad184dc0a62b8457bca53
Parth Viradiya <parthviradiya08@gmail.com>
2020-12-08 12:20:15 -0500

    fixed wrong equation

--- gravity_model.m
+++ gravity_model.m
@@ -6,7 +6,7 @@ function FM_grav = gravity_model(x)
 
 global g;
 
-F_grav = g*[sin(x(8)); sin(x(7))*cos(x(8)); cos(x(7))*cos(x(8))]; % N
+F_grav = g*[-sin(x(8)); sin(x(7))*cos(x(8)); cos(x(7))*cos(x(8))]; % N
 M_grav = [0; 0; 0]; % N*m
 FM_grav = [F_grav; M_grav];
 end
