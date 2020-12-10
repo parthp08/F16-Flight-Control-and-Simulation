d1adb3135d7b9454c1389ff86848dd100cffccb9
Parh Viradiya <parthviradiya08@gmail.com>
2020-12-10 18:34:35 -0500

    minor modification

--- Valphabeta2uvw.m
+++ Valphabeta2uvw.m
@@ -1,12 +1,8 @@
 function [u,v,w] = Valphabeta2uvw(V,alpha,beta)
 % convert V, alpha, beta to u, v, w in body frame
 
-t_alpha = tan(alpha);
-t_beta = tan(beta);
+u = V*cos(alpha)*cos(beta);
+v = V*sin(beta);
+w = V*sin(alpha)*cos(beta);
 
-denomintor = sqrt(1 + (t_alpha*t_alpha) + (t_beta*t_beta));
-
-u = V/denomintor;
-v = V*t_beta/denomintor;
-w = V*t_alpha/denomintor;
 end
